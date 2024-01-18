36eff8ca0dd0c2852ca666a1f629650fef16cf9c
Henry Bishop <hgb209@exeter.ac.uk>
2024-01-18 13:47:16 +0000

    Add base pso cython files with build changes

---
+++ scipy/optimize/_particleswarm.pyx
@@ -0,0 +1,12 @@
+__all__ = ['particleswarm']
+
+class Particle:
+    position = []
+    velocity = []
+    pbest = []
+
+def particleswarm(func, population_size, niter=100, c1=1, c2=1, w=1):
+    pass
+
+
+
