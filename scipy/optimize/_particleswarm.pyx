df44de3062611294b138a87d049dc0d5d08327f0
Henry Bishop <hgb209@exeter.ac.uk>
2024-02-09 15:25:03 +0000

    Added dealing with the bounds (via setting out of bounds particles fintness to infinity

--- scipy/optimize/_particleswarm.pyx
+++ scipy/optimize/_particleswarm.pyx
@@ -85,6 +85,11 @@ cdef class State:
         cdef int i
         cdef int j
 
+        if self.bounds is None:
+            # If no bounds are given, assume the bounds are (-1, 1) for each dimension
+            # TODO: This is not a good way of doing this, as this will eventually constrict the search space even if the user doesn't want it to be
+            self.bounds = np.array([(-1, 1)] * self.dimensions)
+
         for i in range(self.swarmSize):
             for j in range(self.dimensions):
                 # Choose a random position within the bounds for that dimension
@@ -98,7 +103,7 @@ cdef class State:
 
         for i in range(self.swarmSize):
             for j in range(self.dimensions):
-                # Choose a random velocity within the bounds for that dimension
+                # Choose a random velocity between -1 and 1
                 random_velocity = np.random.uniform(-1, 1)
                 # Set the velocity
                 self.velocities[i, j] = random_velocity
@@ -114,6 +119,11 @@ cdef class State:
         # Update the pbest fitness if the new fitness is better
         if fitness < self.pbest_fitnesses[particle_index]:
             self.pbest_fitnesses[particle_index] = fitness
+        # If the new position is out of bounds, set the particles fitness to infinity
+        # This will cause the particle to be ignored in the next iteration but still be in the swarm
+        # with the potential to be reactivated
+        if np.any(self.positions[particle_index] < self.bounds[:, 0]) or np.any(self.positions[particle_index] > self.bounds[:, 1]):
+            self.pbest_fitnesses[particle_index] = np.inf
         return fitness
 
     cdef void calculate_all_fitnesses(self):
@@ -157,8 +167,8 @@ cdef class State:
         cdef np.ndarray velocity = self.velocities[particle_index]
         for i in range(self.dimensions):
             position[i] = position[i] + velocity[i]
-            # TODO: Deal with the bounds
-    
+            # The bounds are dealt with in the fitness function, so we don't need to worry about them here
+
     cdef void update_all_positions(self):
         cdef int i
         for i in range(self.swarmSize):
@@ -199,139 +209,6 @@ cdef class State:
 
         print(f"Finished running")
         self.print_class_variables()
-
-
-
-
-        
-
-class Particle:
-    def __init__(self):
-        self.position = None
-        self.velocity = None
-        self.fitness = None
-        self.pbest_position = None
-        self.pbest_fitness = None
-
-    def initialise_position(self, bounds):
-        x_position = np.random.uniform(bounds[0][0], bounds[0][1])
-        y_position = np.random.uniform(bounds[1][0], bounds[1][1])
-        self.position = [x_position, y_position]
-
-    def initialise_velocity(self):
-        x_velocity = np.random.uniform(-1, 1)
-        y_velocity = np.random.uniform(-1, 1)
-        self.velocity = [x_velocity, y_velocity]
-
-    def calculate_fitness(self, objective_function):
-        self.fitness = objective_function(self.position[0], self.position[1])
-
-    def update_velocity(self, w, c1, c2, gbest_position):
-        for i in range(len(self.velocity)):
-            r1 = np.random.uniform(-1, 1)
-            r2 = np.random.uniform(-1, 1)
-
-            velocity = self.velocity.copy()
-
-            pbest = self.pbest_position.copy()
-            position = self.position.copy()
-
-            gbest_position = gbest_position.copy()
-
-            cognitive_component = c1 * r1 * (pbest[i] - position[i])
-            social_component = c2 * r2 * (gbest_position[i] - position[i])
-
-            self.velocity[i] = w * velocity[i] + cognitive_component + social_component
-
-    def update_position(self, bounds):
-        for i in range(len(self.position)):
-            self.position[i] = self.position[i] + self.velocity[i]
-            if self.position[i] < bounds[i][0]:
-                self.position[i] = bounds[i][0]
-            elif self.position[i] > bounds[i][1]:
-                self.position[i] = bounds[i][1]
-
-    def update_pbest(self):
-        if self.fitness < self.pbest_fitness:
-            self.pbest_fitness = self.fitness.copy()
-            self.pbest_position = self.position.copy()
-
-
-class ParticleSwarm:
-    def __init__(self, objective_function, swarm_size, max_iterations, w, c1, c2, bounds):
-        self.objective_function = objective_function
-        self.swarm_size = swarm_size
-        self.max_iterations = max_iterations
-        self.w = w
-        self.c1 = c1
-        self.c2 = c2
-        self.bounds = bounds
-        self.gbest_position = None
-        self.gbest_fitness = 99999
-        self.swarm = []
-
-        self.plateau = 0
-
-    def initialise_swarm(self):
-        for i in range(self.swarm_size):
-            particle = Particle()
-            particle.initialise_position(self.bounds)
-            particle.initialise_velocity()
-            particle.calculate_fitness(self.objective_function)
-            particle.pbest_position = particle.position
-            particle.pbest_fitness = particle.fitness
-            self.swarm.append(particle)
-
-    def update_gbest(self):
-        for particle in self.swarm:
-            if particle.fitness < self.gbest_fitness:
-                self.gbest_fitness = particle.fitness
-                self.gbest_position = particle.position.copy()
-                # reset plateau counter
-                if self.gbest_fitness != self.objective_function(self.gbest_position[0], self.gbest_position[1]):
-                    print(f"Something funky is going on: {self.gbest_fitness} != ")
-
-                self.plateau = 0
-        # if no improvement, increment plateau counter
-        self.plateau += 1
-
-    def check_termination(self):
-        if self.plateau > 500:
-            return True
-        else:
-            return False
-
-    def run(self):
-        # Do the initialisation steps
-        self.initialise_swarm()
-        self.update_gbest()
-
-        # Run the iterations
-        for i in range(self.max_iterations):
-
-            for particle in self.swarm:
-                particle.calculate_fitness(self.objective_function)
-                particle.update_pbest()
-
-            self.update_gbest()
-
-            for particle in self.swarm:
-                particle.update_velocity(self.w, self.c1, self.c2, self.gbest_position)
-                particle.update_position(self.bounds)
-
-            if self.gbest_fitness != self.objective_function(self.gbest_position[0], self.gbest_position[1]):
-                print(f"Something funky is going on: {self.gbest_fitness} != {self.objective_function(self.gbest_position[0], self.gbest_position[1])}")
-
-            if self.check_termination():
-                print(f'Early termination at iteration {i}\n'
-                      f'Best fitness: {self.gbest_fitness}\n'
-                      f'Best position: {self.gbest_position}')
-                break
-
-            #print("Breakpoint")
-        #print(f"Best fitness and position: {self.gbest_fitness}, {self.gbest_position}")
-        return self.gbest_fitness, self.gbest_position
-
 
 def particleswarm(objective_function, swarm_size, max_iterations, w, c1, c2, dimensions, bounds=None):
     pso = State(objective_function, swarm_size, max_iterations, w, c1, c2, dimensions, bounds)
